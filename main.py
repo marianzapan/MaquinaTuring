c57afefa757f3dfa36082c4aa07e37e4016daad2
marianzapan <mariannyy1002@gmail.com>
2023-05-30 21:00:33 -0700

    Clase

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 from mt import *
 
-qinicial = "q0"
-qaceptacion = ["qfinal"]
-
 #Inversión de bits (igual que en tabla)
 funcionTransicion = {("q0","0"):("1", "R", "q0"),
                      ("q0","1"):("0", "R", "q0"),
                      ("q0"," "):(" ", " ", "qfinal")}
 qfinales = {"qfinal"}
 
-mt = MT("110 ", q0="q0", qfinales=qfinales, funcionTransicion=funcionTransicion)
+mt = MT("1010101011110 ", q0="q0", qfinales=qfinales, funcionTransicion=funcionTransicion)
 
 print("Input en Cinta: \n" + mt.getCinta())
 
--- mt.py
+++ mt.py
@@ -20,13 +20,48 @@ class Cinta:
 
 class MT:
     def __init__(self, cinta ="", simbolo_enblanco = " ", q0 = "", qfinales = None, funcionTransicion = None):
-        pass
+        self.cinta = Cinta(cinta)
+        self.posCabeza = 0
+        self.simbolo_enblanco = simbolo_enblanco
+        self.estadoActual = q0
+
+        if qfinales == None:
+            self.qfinales = set()
+        else:
+            self.qfinales = set(qfinales)
+
+        if funcionTransicion == None:
+            self.funcionTransicion = dict()
+        else:
+            self.funcionTransicion = funcionTransicion
     
     def getCinta(self):
-        pass
+        return self.cinta.cadena()
     
     def step(self):
-        pass
+        #Leer -> escribir -> mover
+        carDebajoCabeza = self.cinta[self.posCabeza] #Leer
+        # t(estado, simbolo/car) = (car, pos, estado)
+        # t(q0, 0) = (1, R, q0)
+        # t(q0, 1) = (0, R, q0)
+        # t(q0, _) = (_, _, qf)
+        x = (self.estadoActual, carDebajoCabeza) #(q0, 1)
+
+        if x in self.funcionTransicion:
+            y = self.funcionTransicion[x]
+            self.cinta[self.posCabeza] = y[0] #Escribir
+
+            #Mover
+            if y[1] == 'R':
+                self.posCabeza += 1 #self.posCabeza = self.posCabeza + 1 
+            elif y[1] == 'L':
+                self.posCabeza -= 1 #self.posCabeza = self.posCabeza - 1 
+            
+            #Pasar al siguiente estado
+            self.estadoActual = y[2]
     
     def final(self):
-        pass
+        if self.estadoActual in self.qfinales: #Si es estado final, se termina
+            return True
+        else:
+            return False
